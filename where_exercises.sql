712ca12b820b06279beb920c89b1d55bacd6b12c
Ian Crane <ian.crane12@gmail.com>
2022-10-25 14:10:37 -0500

    optimizing commands

--- where_exercises.sql
+++ where_exercises.sql
@@ -12,11 +12,11 @@ SELECT first_name, last_name FROM employees WHERE last_name LIKE ('E%e');
 -- 899
 SELECT COUNT(*) FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 -- 135214
-SELECT first_name, last_name, birth_date FROM employees WHERE birth_date LIKE '%-12-15';
+SELECT COUNT(*) FROM employees WHERE birth_date LIKE '%-12-15';
 -- 842
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31');
+SELECT COUNT(*) FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%';
 -- 362
-SELECT first_name, last_name FROM employees WHERE last_name LIKE ('%q%');
+SELECT COUNT(*) FROM employees WHERE last_name LIKE ('%q%');
 -- 1873
-SELECT first_name, last_name FROM employees WHERE last_name LIKE ('%q%') AND last_name NOT LIKE ('%qu%');
+SELECT COUNT(*) FROM employees WHERE last_name LIKE ('%q%') AND last_name NOT LIKE ('%qu%');
 -- 547
