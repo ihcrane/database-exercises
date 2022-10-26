6969e08f476b996c813867bd17936089fd099040
Ian Crane <ian.crane12@gmail.com>
2022-10-26 15:17:37 -0500

    Finished bonus questions

--- join_exercises.sql
+++ join_exercises.sql
@@ -2,6 +2,8 @@ SELECT users.name as user_name, roles.name as role_name, users.email as emails, 
  FROM users JOIN roles on users.role_id = roles.id GROUP BY role_name, user_name, emails;
 
  -- EMPLOYEES DB
+
+ --Q2
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -13,6 +15,7 @@ JOIN titles AS t
 WHERE t.title = 'Manager' AND t.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+--Q3
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -24,7 +27,7 @@ JOIN titles AS t
 WHERE title = 'Manager' AND t.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY d.dept_name;
 
-
+--Q4
 SELECT t.title, COUNT(e.emp_no)
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -38,6 +41,7 @@ JOIN titles AS t
 WHERE d.dept_name = 'Customer Service' AND dm.to_date = '9999-01-01'
 GROUP BY t.title;
 
+--Q5
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -51,7 +55,7 @@ JOIN salaries AS s
 WHERE title = 'Manager' AND t.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
-
+--Q6
 SELECT d.dept_name, COUNT(de.emp_no)
 FROM departments AS d 
 JOIN dept_emp AS de
@@ -61,6 +65,7 @@ JOIN employees AS e
 GROUP BY d.dept_name
 ORDER BY d.dept_name;
 
+--Q7
 SELECT d.dept_name, AVG(s.salary)
 FROM departments AS d 
 JOIN dept_emp AS de
@@ -73,6 +78,7 @@ WHERE de.to_date >= NOW()
 GROUP BY d.dept_name
 ORDER BY AVG(s.salary) DESC LIMIT 1;
 
+--Q8
 SELECT e.first_name, e.last_name
 FROM departments AS d 
 JOIN dept_emp AS de
@@ -84,6 +90,7 @@ JOIN salaries AS s
 WHERE d.dept_name = 'Marketing'
 ORDER BY s.salary DESC LIMIT 1;
 
+--Q9
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary,
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -97,6 +104,7 @@ JOIN salaries AS s
 WHERE title = 'Manager' AND t.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 ORDER BY s.salary DESC LIMIT 1;
 
+--Q10
 SELECT d.dept_name, ROUND(AVG(s.salary), 0)
 FROM departments AS d 
 JOIN dept_emp AS de
@@ -108,18 +116,7 @@ JOIN salaries AS s
 GROUP BY d.dept_name
 ORDER BY AVG(s.salary) DESC;
 
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS employee_name, d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name
-FROM departments AS d 
-JOIN dept_manager AS dm
-	ON d.dept_no = dm.dept_no
-JOIN dept_emp AS de
-	ON dm.dept_no = de.dept_no
-JOIN employees AS e
-	ON de.emp_no = e.emp_no
-JOIN titles AS t
-	ON t.emp_no = e.emp_no
-WHERE t.title = 'Manager' AND t.to_date = '9999-01-01';
-
+--Q11: Bonus 1
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS employee_name, d.dept_name, CONCAT(em.first_name, ' ', em.last_name)
 FROM departments AS d 
 JOIN dept_manager AS dm
@@ -132,3 +129,15 @@ JOIN employees AS em
 	ON dm.emp_no = em.emp_no
 WHERE dm.to_date = '9999-01-01' AND dm.emp_no = em.emp_no
 GROUP BY employee_name, d.dept_name, dm.emp_no;
+
+--Q12: Bonus 2
+SELECT d.dept_name, MAX(s.salary) AS highest_salary
+FROM departments AS d 
+JOIN dept_emp AS de
+	ON d.dept_no = de.dept_no
+JOIN employees AS e
+	ON de.emp_no = e.emp_no
+JOIN salaries AS s
+	ON e.emp_no = s.emp_no
+WHERE s.to_date >= NOW()
+GROUP BY d.dept_name;
